b873cb3ae5133bf0ee2b179a502a0aa79f96d68d
moroztaras <moroztaras@i.ua>
2015-02-13 18:35:37 +0200

    fixtures data for table Blog

---
+++ src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Blogger\BlogBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Blogger\BlogBundle\Entity\Blog;
+
+class BlogFixtures implements FixtureInterface
+{
+    /*
+     * Load Data
+     */
+    public function load(ObjectManager $manager)
+    {
+        $blog1 = new Blog();
+        $blog1->setTitle('A day with blogger 2015');
+        $blog1->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing eletra electrify denim vel ports.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut velocity magna. Etiam vehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras el mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis, justo mauris lacinia libero, non facilisis purus ipsum non mi. Aliquam sollicitudin, augue id vestibulum iaculis, sem lectus convallis nunc, vel scelerisque lorem tortor ac nunc. Donec pharetra eleifend enim vel porta.');
+        $blog1->setImage('img1.jpg');
+        $blog1->setAuthor('Мороз Тарас');
+        $blog1->setTags('symfony2, php, paradise, blogger');
+        $blog1->setCreated(new \DateTime());
+        $blog1->setUpdated($blog1->getCreated());
+        $manager->persist($blog1);
+
+        $blog2 = new Blog();
+        $blog2->setTitle('The pool on the roof must have a leak');
+        $blog2->setBlog('Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Na. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis.');
+        $blog2->setImage('img2.jpg');
+        $blog2->setAuthor('Мороз Тарас');
+        $blog2->setTags('pool, leaky, hacked, movie, hacking, blogger');
+        $blog2->setCreated(new \DateTime("2015-02-13 08:02:00"));
+        $blog2->setUpdated($blog2->getCreated());
+        $manager->persist($blog2);
+
+        $blog3 = new Blog();
+        $blog3->setTitle('Misdirection. What the eyes see and the ears hear, the mind believes');
+        $blog3->setBlog('Lorem ipsumvehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque.');
+        $blog3->setImage('img3.jpg');
+        $blog3->setAuthor('Мороз Тарас');
+        $blog3->setTags('misdirection, magic, movie, hacking, blogger');
+        $blog3->setCreated(new \DateTime("2015-02-13 08:03:00"));
+        $blog3->setUpdated($blog3->getCreated());
+        $manager->persist($blog3);
+
+        $blog4 = new Blog();
+        $blog4->setTitle('The grid - A digital frontier');
+        $blog4->setBlog('Lorem commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra.');
+        $blog4->setImage('img4.jpg');
+        $blog4->setAuthor('Kevin Flynn');
+        $blog4->setTags('grid, daftpunk, movie, blogger');
+        $blog4->setCreated(new \DateTime("2015-02-13 08:04:00"));
+        $blog4->setUpdated($blog4->getCreated());
+        $manager->persist($blog4);
+
+        $blog5 = new Blog();
+        $blog5->setTitle('You\'re either a one or a zero. Alive or dead');
+        $blog5->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing elittibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque.');
+        $blog5->setImage('img5.jpg');
+        $blog5->setAuthor('Gary Winston');
+        $blog5->setTags('binary, one, zero, alive, dead, !trusting, movie, blogger');
+        $blog5->setCreated(new \DateTime("2015-02-13 08:05:00"));
+        $blog5->setUpdated($blog5->getCreated());
+        $manager->persist($blog5);
+
+        $manager->flush();
+    }
+
+}
