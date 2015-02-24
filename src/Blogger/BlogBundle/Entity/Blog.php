d0ecbac2ca486e481ac4018ac24b00bf2f9dff80
moroztaras <moroztaras@i.ua>
2015-02-24 14:19:15 +0200

    added a constructor to set default values for members created and updated

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="blog")
+ * @ORM\HasLifecycleCallbacks
  */
 class Blog
 {
@@ -53,6 +54,20 @@ class Blog
      * @ORM\Column(type="datetime")
      */
     protected $updated;
+
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedValue()
+    {
+        $this->setUpdated(new \DateTime());
+    }
 
     /**
      * Get id
