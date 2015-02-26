02af5963810dea30f1decc8575fc23e44e776361
moroztaras <moroztaras@i.ua>
2015-02-26 14:17:07 +0200

    added views comments for blog

--- src/Blogger/BlogBundle/Controller/BlogController.php
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -21,7 +21,12 @@ class BlogController extends Controller
         if (!$blog) {
             throw $this->createNotFoundException('Не вдалося знайти вказаного поста у блозі.');
         }
+        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+                       ->getCommentsForBlog($blog->getId());
 
-        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array('blog'=> $blog));
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+            'comments'  => $comments
+        ));
     }
 }
