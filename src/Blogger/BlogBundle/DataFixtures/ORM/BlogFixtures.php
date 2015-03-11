c5e6bde33a2589d2d788e8ea6f48467c8c2fa20b
moroztaras <moroztaras@i.ua>
2015-03-11 14:12:37 +0200

    fixtures data for entity Blog in database

--- src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -7,7 +7,6 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Blogger\BlogBundle\Entity\Blog;
 
-
 class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
@@ -17,7 +16,7 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $blog1->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing eletra electrify denim vel ports.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut velocity magna. Etiam vehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras el mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis, justo mauris lacinia libero, non facilisis purus ipsum non mi. Aliquam sollicitudin, augue id vestibulum iaculis, sem lectus convallis nunc, vel scelerisque lorem tortor ac nunc. Donec pharetra eleifend enim vel porta.');
         $blog1->setImage('img1.jpg');
         $blog1->setAuthor('Moroz Taras');
-        $blog1->setTags('symfony2, php, paradise, symblog');
+        $blog1->setTags('symfony2, php, paradise, blogger 2015');
         $blog1->setCreated(new \DateTime());
         $blog1->setUpdated($blog1->getCreated());
         $manager->persist($blog1);
@@ -27,7 +26,7 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $blog2->setBlog('Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Na. Cras elementum molestie vestibulum. Morbi id quam nisl. Praesent hendrerit, orci sed elementum lobortis.');
         $blog2->setImage('img2.jpg');
         $blog2->setAuthor('Moroz Taras');
-        $blog2->setTags('pool, leaky, hacked, movie, hacking, symblog');
+        $blog2->setTags('pool, leaky, hacked, movie, hacking, blogger 2015');
         $blog2->setCreated(new \DateTime());
         $blog2->setUpdated($blog2->getCreated());
         $manager->persist($blog2);
@@ -37,7 +36,7 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $blog3->setBlog('Lorem ipsumvehicula nunc non leo hendrerit commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque.');
         $blog3->setImage('img3.jpg');
         $blog3->setAuthor('Moroz Taras');
-        $blog3->setTags('misdirection, magic, movie, hacking, symblog');
+        $blog3->setTags('misdirection, magic, movie, hacking, blogger 2015');
         $blog3->setCreated(new \DateTime());
         $blog3->setUpdated($blog3->getCreated());
         $manager->persist($blog3);
@@ -47,7 +46,7 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $blog4->setBlog('Lorem commodo. Vestibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur tempus nisl vitae viverra.');
         $blog4->setImage('img4.jpg');
         $blog4->setAuthor('Moroz Taras');
-        $blog4->setTags('grid, daftpunk, movie, symblog');
+        $blog4->setTags('grid, daftpunk, movie, blogger 2015');
         $blog4->setCreated(new \DateTime());
         $blog4->setUpdated($blog4->getCreated());
         $manager->persist($blog4);
@@ -57,7 +56,7 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $blog5->setBlog('Lorem ipsum dolor sit amet, consectetur adipiscing elittibulum vulputate mauris eget erat congue dapibus imperdiet justo scelerisque.');
         $blog5->setImage('img5.jpg');
         $blog5->setAuthor('Moroz Taras');
-        $blog5->setTags('binary, one, zero, alive, dead, !trusting, movie, symblog');
+        $blog5->setTags('binary, one, zero, alive, dead, !trusting, movie, blogger 2015');
         $blog5->setCreated(new \DateTime());
         $blog5->setUpdated($blog5->getCreated());
         $manager->persist($blog5);
@@ -70,9 +69,9 @@ class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface
         $this->addReference('blog-4', $blog4);
         $this->addReference('blog-5', $blog5);
     }
+
     public function getOrder()
     {
         return 1;
     }
-
 }
