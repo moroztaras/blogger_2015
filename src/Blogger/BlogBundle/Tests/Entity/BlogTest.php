367e6dd295e07c8af4241562fea570de4536048f
moroztaras <moroztaras@i.ua>
2015-03-17 12:48:03 +0200

    simple version of the test

--- src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
+++ src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
@@ -6,5 +6,9 @@ use Blogger\BlogBundle\Entity\Blog;
 
 class BlogTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testSlugify()
+    {
+        $blog = new Blog();
+        $this->assertEquals('hello-world', $blog->slugify('Hello World'));
+    }
 }
