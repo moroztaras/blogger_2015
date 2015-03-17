ad10aeb7f253787cfbc84030b9a7d019ed6503f7
moroztaras <moroztaras@i.ua>
2015-03-17 13:08:49 +0200

    successful test

--- src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
+++ src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
@@ -11,5 +11,8 @@ class BlogTest extends \PHPUnit_Framework_TestCase
         $blog = new Blog();
         $this->assertEquals('hello-world', $blog->slugify('Hello World'));
         $this->assertEquals('hi-moroz-taras', $blog->slugify('Hi Moroz Taras'));
+        $this->assertEquals('hello-world', $blog->slugify('Hello    world'));
+        $this->assertEquals('blogger', $blog->slugify('blogger '));
+        $this->assertEquals('blogger', $blog->slugify(' blogger'));
     }
 }
