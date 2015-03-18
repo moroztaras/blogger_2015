7e2685ad0bd11a642f22e6755fbd75e3cdc86858
moroztaras <moroztaras@i.ua>
2015-03-18 13:04:53 +0200

    method for checking the homepage

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -14,4 +14,14 @@ class PageControllerTest extends WebTestCase
 
         $this->assertEquals(1, $crawler->filter('h1:contains("Про автора Blogger 2015")')->count());
     }
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        // Check there are some blog entries on the page
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+    }
 }
