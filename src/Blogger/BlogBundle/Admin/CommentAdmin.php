ff27a46d1c5f63649a8b96053b30e74fe20f09d4
moroztaras <moroztaras@i.ua>
2015-03-26 18:56:21 +0200

    a new class for comment from the admin panel

---
+++ src/Blogger/BlogBundle/Admin/CommentAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Blogger\BlogBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CommentAdmin extends Admin
+{
+
+}
